b98c4954abf2f59d10a97ca280413678b949d7df
adiba <adibahossainn@gmail.com>
2025-05-16 17:47:38 -0600

    added map in map page

--- travel.js
+++ travel.js
@@ -1,9 +1,7 @@
 
-const map = L.map('map').setView([3,3], 2);
+const map = L.map('map').setView([0,0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-//<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
