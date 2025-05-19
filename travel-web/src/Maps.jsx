791a739f1e0bf3c619d73064c2607df4b491856c
adiba <adibahossainn@gmail.com>
2025-05-19 17:01:39 -0600

    resized map and added custom marker

--- travel-web/src/Maps.jsx
+++ travel-web/src/Maps.jsx
@@ -1,19 +1,28 @@
 
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+
 
 
 function Maps(){
 
     return(
 
-    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
+    <>
+    <h1 className='mapsTitle'>Places Visited</h1>
+
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
     <Map
-        style = {{width: '50vw', height: '50vh'}}
-        Center={{ lat: 36.365, lng: 7.565,}}
+
+        style = {{width: '50vw', height: '60vh', margin: 'auto', padding: '50px'}}
+        defaultCenter={{ lat: 38, lng: 14,}}
         defaultZoom={2}
         disableDefaultUI={true}
       />    
+    <Marker position= {{ lat: 55.079, lng: -103.163  }} icon={{ url: '/pin.png', scaledSize: {width: 40, height: 40},}}/>
+   
+    
   </APIProvider>
+  </>
   );
 }
 
